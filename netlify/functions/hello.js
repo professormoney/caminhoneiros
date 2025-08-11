81c05b32160a11eb3735925d52255c9fac256b32
professormoney <professormoney@protonmail.com>
2025-08-11 17:33:02 -0300

    Delete netlify/functions/hello.js

--- netlify/functions/hello.js
+++
@@ -1,7 +0,0 @@
-export async function handler(event) {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ok: true, method: event.httpMethod })
-  };
-}
